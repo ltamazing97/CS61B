fcb10271282115642f875e1f41021ca222ffa7ad
Tao Liu <isgeorgeliu@gmail.com>
2019-07-26 21:18:10 +0800

    complete proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -17,6 +17,7 @@ public class TestOffByOne {
 		assertFalse(offByOne.equalChars('a', 'a'));
 		assertFalse(offByOne.equalChars('a', 'e'));
 		assertFalse(offByOne.equalChars('z', 'a'));
+		assertFalse(offByOne.equalChars('A', 'a'));
 	}
 
 }
